db4cae079dbece462f51dbab701b471c3853893e
Ranger Harke <ranger.harke@gmail.com>
2020-01-26 00:26:09 -0500

    Fix sample script

--- chime.sh
+++ chime.sh
@@ -2,4 +2,4 @@
 
 # This is just an example of script that you could run in response to a serial event
 
-/usr/bin/curl --silent --output --max-time 5 /dev/null http://192.168.0.50:8000/chime?type=doorbell &
+/usr/bin/curl --silent --output /dev/null --max-time 5 http://192.168.0.50:8000/chime?type=doorbell &
